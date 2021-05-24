9b92fb08f9e754908ce4d4ae8d58636d9d9ec29c
Arnaldo Pagani <arnaldopaganijr@gmail.com>
2021-05-24 19:13:09 -0300

    primeiro commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace atividade_i
+{
+    class Program
+    {
+        static list<itemPedido> pedido;
+        static void Main(string[] args)
+        {
+            
+        }
+        public class itemPedido(){
+
+            string descicao;
+            double valorUnitario;
+            int quantidade;
+
+
+        }
+        public class pedido(){
+
+            pedido = new list<itemPedido>();
+            pedido.Add(new itemPedido());
+            pedido.Add(new itemPedido());
+            pedido.Add(new itemPedido());
+            pedido.Add(new itemPedido());
+            pedido.Add(new itemPedido());
+
+            foreach (itemPedido p in pedido)
+            {
+                console.writeLine(p.descicao + "" + p.quantidade);
+            }
+        }
+    }
+}
